99d0258f43723f39c3be4ee5db556ed928451792
hmquyuef <hmquyuef@uef.edu.vn>
2024-09-23 20:55:02 +0700

    update code

--- DemoConsole/Program.cs
+++ DemoConsole/Program.cs
@@ -1,123 +1,105 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DemoConsole
 {
-    //viết ở đây
-    internal class Program
+    class Program
     {
         //viết ở đây
+        public static void NhanVien(string hoten, int lcb, float hsl = 2f)
+        {
+            Console.WriteLine("Ho ten la {0}", hoten);
+            Console.WriteLine("LCB la {0}", lcb);
+            Console.WriteLine("HSL la {0}", hsl);
+            Console.WriteLine("Thuc lanh la {0}", lcb * hsl);
+        }
+        //public static int TongMang(int a1, int a2, int a3, int a4, .... int a10)
+        //{
+        //    return a1 + a2 + a3 + ... + a10;
+        //}
+
+        public static int TongMang(params int[] a)
+        {
+            int s = 0;
+            for(int i = 0; i < a.Length; i++)
+            {
+                s += a[i];
+            }
+            bool KiemTraSoChan()
+            {
+                if (s % 2 == 0) return true;
+                else return false;
+            }
+            return s;
+        }
         static void Main(string[] args)
         {
-            //Bai 1: Ctrl + K, Ctrl + C
-            //Ctrl + K, Ctrl + U
-            //int A = 9;
-            //if (A % 2 == 0) Console.WriteLine("{0} la so chan", A);
-            //else Console.WriteLine("{0} la so le", A);
-            //Bai 2:
-
-            //int B = 10;
-            //string kq = "so chan";
-            //if (B % 2 != 0) kq = "so le";
-
-            //Console.WriteLine("{0} la {1}", B, kq);
-
-            //int C = 10;
-            //Console.WriteLine("{0} la {1}", C, C % 2 == 0 ? "so chan" : "so le");
-
-            //int D = 11;
-            //if(D % 2 == 0) Console.WriteLine("{0} la so chan", D);
-            //if(D % 2 != 0) Console.WriteLine("{0} la so le", D);
-
-            //string E = null;
-            //string F = E ?? "DEMO"; //if loai 1
-            //Console.WriteLine(F);
-            //if(soCanh == 3) Console.WriteLine("Hinh tam giac");
-            //else if(soCanh >= 4) Console.WriteLine("Hinh chu nhat");
-            //else if(soCanh == 5) Console.WriteLine("Hinh chu nhat");
-            //else if(soCanh == 6) Console.WriteLine("Hinh chu nhat");
-            //else if(soCanh == 7) Console.WriteLine("Hinh chu nhat");
-            //else if(soCanh == 8) Console.WriteLine("Hinh chu nhat");
-            //else if(soCanh == 9) Console.WriteLine("Hinh chu nhat");
-            //else if(soCanh == 10) Console.WriteLine("Hinh chu nhat");
-            //else Console.WriteLine("ko hop le");
-
-            //int soCanh = 4;
-            //switch (soCanh)
+            Array.Resize(ref a,7);
+            List<int> b = new List<int>();
+            ArrayList c = new ArrayList();
+            int[] a = new int[5];
+            a[6] = 10;
+            Console.Write("{0}\t", a[6]);
+            //int[] numbers = { 5, 7, 1, 9, 5, 2, 4, 3, 6, 2, 10 };
+            //Array.Sort(numbers);
+            //Array.Reverse(numbers);
+            //int mã = numbers.Max();
+            //for (int i = 0; i < numbers.Length; i++)
             //{
-            //    //if(soCanh == 3 || soCanh == 4 || soCanh == 5)
-            //    case 3: 
-            //    case 4:
-            //    case 5:
-            //        Console.WriteLine("Hinh chu nhat");
-            //        break;
-            //    case 6:
-            //        Console.WriteLine("Hinh chu nhat");
-            //        break;
-            //    default:
-            //        Console.WriteLine("ko hop le");
-            //        break;
+            //    Console.Write("{0}\t", numbers[i]);
             //}
 
-            //Console.WriteLine("1");
-            //Console.WriteLine("2");
-            //Console.WriteLine("3");
-            //Console.WriteLine("4");
-            //Console.WriteLine("5");
-            //Console.WriteLine("6");
-            //Console.WriteLine("7");
-            //Console.WriteLine("8");
-            //Console.WriteLine("9");
-            //Console.WriteLine("10");
+            //int tong = 0;
+            //for (int i = 0; i < numbers.Length; i++) tong += numbers[i];
 
-            //int s = 0;
-
-            //for (int i = 1; i <= 100; i++)//1000 lan
+            //int sum = 0;
+            //foreach (var number in numbers)
             //{
-            //    if (i % 2 != 0) s += i;
+            //    if (number % 3 == 0) Console.WriteLine(number);
+            //    break;
             //}
-            //Console.WriteLine("Tong la: {0}", s);
 
-            //for (int i = 2; i <= 1000; i += 2) // i = i + 1 // 500 lan
-            //{
-            //    s += i;
-            //}
-            //Console.WriteLine(s);
 
-            //for (int i = 1; i <= 100; i++) // i = i + 1 // 500 lan
+            //int max = numbers[0];
+            //for (int i = 1; i < numbers.Length; i++) if (max < numbers[i]) max = numbers[i];
+            //Console.WriteLine("So lon nhat trong mang la {0}", max);
+
+            //int MAX = numbers.Max();
+            //int MIN = numbers.Min();
+            //double av = numbers.Average();
+            //Console.WriteLine("So lon nhat trong mang la {0}", MAX);
+
+            //while (true)
             //{
-            //    for (int j = 1; j <= 100; j++) // i = i + 1 // 500 lan
+            //    Console.Write("Nhap so luong phan tu: ");
+            //    int n = int.Parse(Console.ReadLine());
+
+            //    int[] mangSoNguyen = new int[n]; // chay tu 0 -> n-1
+            //                                     //nhap gia tri cho mang
+            //    for (int i = 0; i < mangSoNguyen.Length; i++)
             //    {
-            //        for (int z = 1; z <= 100; z++) // i = i + 1 // 500 lan
-            //        {
-            //            Console.WriteLine("{0} x {1} x {2} = {3}", i, j, z, i * j * z);
-            //        }
-            //        Console.WriteLine();
+            //        Console.Write("Gia tri phan tu {0} = ", i);
+            //        mangSoNguyen[i] = int.Parse(Console.ReadLine());
             //    }
+
+            //    //xu ly mang
+
+
             //    Console.WriteLine();
+            //    Console.WriteLine("Gia tri mang vua nhap:");
+            //    //xuat gia tri cho mang
+            //    for (int i = 0; i < n; i++)
+            //    {
+            //        Console.Write("{0}\t", mangSoNguyen[i]);
+            //    }
+
+            //    Console.WriteLine("Nhan y de thoat!");
+            //    string stop = Console.ReadLine();
+            //    if (stop == "y" && stop == "Y") break;
+            //    //Console.Clear();
             //}
-
-            for (int j = 1; j <= 100; j++)
-            {
-                if (j % 2 != 0)
-                {
-                    continue;
-                    Console.WriteLine(j);
-                }
-                Console.WriteLine(j);
-            }
-
-            //int i = 1;
-            //while(i <= 100)
-            //{
-            //    if (i % 2 != 0)
-            //        Console.WriteLine(i);
-            //    i++;
-            //}
-
 
             Console.ReadKey();
         }
